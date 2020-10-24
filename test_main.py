3cad35dab710f1db7ac6f88ec4394c62ffd53006
Kaedone <56879035+Kaedone@users.noreply.github.com>
2020-10-24 22:52:48 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,3 +1,13 @@
+import codewars_test as test
+from solution import remove_parentheses
+from random import choice, randint
+from re import sub
+
+def correct(s):
+    while '(' in s:
+        s = sub('\([^()]*\)', '', s)
+    return s
+
 @test.describe("remove parentheses")
 def tests():
     @test.describe("basic tests")
@@ -8,3 +18,13 @@ def tests():
         test.assert_equals(remove_parentheses("a(b(c))"), "a")
         test.assert_equals(remove_parentheses("hello example (words(more words) here) something"), "hello example  something")
         test.assert_equals(remove_parentheses("(first group) (second group) (third group)"), "  ")
+    @test.describe("random tests")
+    def random():
+        for loop in range(100):
+            chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ   '
+            inp_str = ''.join(choice(chars) for char in range(randint(1, 500)))
+            par_num = randint(1, 25)
+            for par in range(par_num):
+                rand_ind = randint(0, len(inp_str) - 1)
+                inp_str = inp_str[:rand_ind] + f"({''.join(choice(chars) for char in range(randint(0, 25)))})" + inp_str[rand_ind:]
+            test.assert_equals(remove_parentheses(inp_str[:]), correct(inp_str[:]))
